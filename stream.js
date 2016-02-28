701dce0c412e1b760f3e1fae78c5f22ec45270ac
Clive Chan <cc@clive.io>
2016-02-28 11:02:06 -0500

    stream pipes to stdout

--- stream.js
+++ stream.js
@@ -1,10 +1,8 @@
 var BinaryServer = require("binaryjs").BinaryServer;
-var fs = require("fs");
 
 var server = BinaryServer({port: 9375});
 server.on('connection', function(client){
   client.on('stream', function(stream, meta){
-    var file = fs.createWriteStream(meta.file);
-    stream.pipe(file);
+    stream.pipe(process.stdout);
   }); 
 });
